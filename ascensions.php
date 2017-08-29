4a29c646f791c787ac53a6ef899e48262b2c610c
dicky <kidoblivious@gmail.com>
2017-08-29 21:28:37 +0100

    Reformatting; a header tag change.

--- ascensions.php
+++ ascensions.php
@@ -1,38 +1,44 @@
-<html>
+<!DOCTYPE html>
 
-<head>
-<?php require_once 'header.inc'; ?>
+<html lang="en">
 
-</head>
-<body>
-<?php
-require_once 'kolchar.inc';
+  <head>
+    <?php require_once 'header.inc'; ?>
+  </head>
 
-$dir = $_GET[ 'dir' ];
+  <body>
+    <?php
+      require_once 'kolchar.inc';
 
-if (ereg ( "20[0-9]{2}[0,1]{1}[0-9]{1}_w[0-9]", $dir ) ) {
-  $target = opendir ( $dir . "/ascensions/" );
-  if ( readdir($target) ) {
-    rewinddir($target);
-    echo "<table border=1> <tr> <td> <b> Name </b> </td><td> <b> KoLDB Link </b> </td> </tr>";
-    $data = array();
-    while ($file = readdir ($target) ) {
-      if ($file != "." && $file != "..") {
-	$char = new kolchar( $file, $dir . "/ascensions" );
-	$data[ $char->get_num() ] = $char;
-      }
-    }
-    sort($data);
-    foreach ( $data as $char ) {
-      $anchor = "<a href=\"$dir/ascensions/" . urlencode($char->get_filename()) . "\"> ". $char->get_name() . " </a>";
-      $dbanchor = "<a href=\"" . $char->getdb() . "\">" . $char->get_num() . "</a>";
-      echo "<tr> <td> $anchor </td> <td> <center> $dbanchor </center> </td> </tr> \n";  
-    }
-    echo "</table>";
-  }
-  closedir($target);
-} else echo "No records seem to exist here.";
+      $dir = $_GET[ 'dir' ];
 
-?>
-</body>
+      if (ereg ( "20[0-9]{2}[0,1]{1}[0-9]{1}_w[0-9]", $dir ) ) {
+        $target = opendir ( $dir . "/ascensions/" );
+        if ( readdir($target) ) {
+          rewinddir($target);
+
+          echo "<table border=1> <tr> <td> <b> Name </b> </td><td> <b> KoLDB Link </b> </td> </tr>";
+
+          $data = array();
+          while ($file = readdir ($target) ) {
+            if ($file != "." && $file != "..") {
+            	$char = new kolchar( $file, $dir . "/ascensions" );
+            	$data[ $char->get_num() ] = $char;
+            }
+          }
+          sort($data);
+          foreach ( $data as $char ) {
+            $anchor = "<a href=\"$dir/ascensions/" . urlencode($char->get_filename()) . "\"> ". $char->get_name() . " </a>";
+            $dbanchor = "<a href=\"" . $char->getdb() . "\">" . $char->get_num() . "</a>";
+
+            echo "<tr> <td> $anchor </td> <td> <center> $dbanchor </center> </td> </tr> \n";
+
+          }
+          echo "</table>";
+        }
+        closedir($target);
+      } else echo "No records seem to exist here.";
+    ?>
+  </body>
+  
 </html>
--- index.php
+++ index.php
@@ -1,79 +1,83 @@
-<html>
-<head>
-<?php require_once 'header.inc'; ?>
+<!DOCTYPE html>
 
-</head>
-<body>
-<h1>
-The <?php print $clan ?> Clan Snapshots
-</h1>
+<html lang="en">
 
-<table border=2>
-<?php
-function prettify( $ugly ) {
-  $week = "week " . $ugly[strlen($ugly)-1];
-  $month = $ugly[4] . $ugly[5];
-  $bgcolor = array();
-  $bgcolour[red] = 0;
-  $bgcolour[green] = 256;
-  $bgcolour[blue] = 0;
-  $bgcolour[green] = $bgcolour[green] - ($month * 11) ;
-  $bgcolour[blue] = $bgcolour[blue] + ($month * 21) ;
-  $months = array( 1 => "January", "February", "March", "April",
-		   "May", "June", "July", "August",
-		   "September", "October", "November", "December");
-  $month =  $months[(int)($month)];
+  <head>
+    <?php require_once 'header.inc'; ?>
+  </head>
 
-  $year = $ugly[3] + 2000;
-  
-  $pretty = "<td bgcolor=" . sprintf('"#%02X%02X%02X"', $bgcolour[red], $bgcolour[green], $bgcolour[blue]) . ">$month $year $week </td>";
-  return $pretty;
-}
+  <body>
 
-$dirs = scandir(".", 1 );
-$relevant_dirs = array();
+    <h1>
+      The <?php print $clan ?> Clan Snapshots
+    </h1>
 
-$i = 0;
-foreach ($dirs as $dir) {
-  if (is_dir($dir) && $dir[0] != ".") {
-    $relevant_dirs[$i++] = $dir;
-  }
-}
-rsort ($relevant_dirs, SORT_STRING);
+    <table border=2>
+      <?php
+      function prettify( $ugly ) {
+        $week = "week " . $ugly[strlen($ugly)-1];
+        $month = $ugly[4] . $ugly[5];
+        $bgcolor = array();
+        $bgcolour[red] = 0;
+        $bgcolour[green] = 256;
+        $bgcolour[blue] = 0;
+        $bgcolour[green] = $bgcolour[green] - ($month * 11) ;
+        $bgcolour[blue] = $bgcolour[blue] + ($month * 21) ;
+        $months = array( 1 => "January", "February", "March", "April",
+      		   "May", "June", "July", "August",
+      		   "September", "October", "November", "December");
+        $month =  $months[(int)($month)];
 
-foreach ($relevant_dirs as $dir ) {
-  $pretty_dir = prettify( $dir );
-  $anchor = '<a href="' . $dir;
-  $hardcore = 'Hardcore';
-  $softcore = 'Softcore';
-  $breakdown = 'Breakdown';
-  $profiles = 'Profiles';
-  $ascensions = 'Ascensions';
-  if (file_exists( $dir.'/hardcore.htm' )) {
-    $hardcore = $anchor . '/hardcore.htm"> Hardcore </a>';
-  }
-  if (file_exists($dir.'/softcore.htm')) {
-    $softcore = $anchor . '/softcore.htm"> Softcore </a>';
-  }
-  if (file_exists( $dir.'/standard.htm')) {
-    $breakdown = $anchor . '/standard.htm"> Breakdown </a>'; 
-  } 
-  $pro_dir = opendir( "$dir/profiles" );
-  $no_profiles = 0;
-  while ($file = readdir($pro_dir) ) {
-    if ($file[0] != "." ) { 
-      $no_profiles++;
-    }
-  }
-  closedir($pro_dir);
-  $profiles = '<a href="profiles.php?dir=' . $dir .'"> Profiles ' . "($no_profiles)" . '</a>';
-  $ascensions = '<a href="ascensions.php?dir=' . $dir . '"> Ascensions </a>';
-  print( "<tr align=\"center\">  $pretty_dir <td> $hardcore </td> <td> $softcore </td> <td> $breakdown </td> <td> $profiles </td> <td> $ascensions</td> </tr>\n");
-}
+        $year = $ugly[3] + 2000;
+        
+        $pretty = "<td bgcolor=" . sprintf('"#%02X%02X%02X"', $bgcolour[red], $bgcolour[green], $bgcolour[blue]) . ">$month $year $week </td>";
+        return $pretty;
+      }
 
-?>
-</table>
+      $dirs = scandir(".", 1 );
+      $relevant_dirs = array();
 
-</body>
-<!-- php hand crafted by Dicky/Gleamglade, 20/08/08 -->
+      $i = 0;
+      foreach ($dirs as $dir) {
+        if (is_dir($dir) && $dir[0] != ".") {
+          $relevant_dirs[$i++] = $dir;
+        }
+      }
+      rsort ($relevant_dirs, SORT_STRING);
+
+      foreach ($relevant_dirs as $dir ) {
+        $pretty_dir = prettify( $dir );
+        $anchor = '<a href="' . $dir;
+        $hardcore = 'Hardcore';
+        $softcore = 'Softcore';
+        $breakdown = 'Breakdown';
+        $profiles = 'Profiles';
+        $ascensions = 'Ascensions';
+        if (file_exists( $dir.'/hardcore.htm' )) {
+          $hardcore = $anchor . '/hardcore.htm"> Hardcore </a>';
+        }
+        if (file_exists($dir.'/softcore.htm')) {
+          $softcore = $anchor . '/softcore.htm"> Softcore </a>';
+        }
+        if (file_exists( $dir.'/standard.htm')) {
+          $breakdown = $anchor . '/standard.htm"> Breakdown </a>'; 
+        } 
+        $profiles_dir = opendir( "$dir/profiles" );
+        $no_profiles = 0;
+        while ($file = readdir($profiles_dir) ) {
+          if ($file[0] != "." ) { 
+            $no_profiles++;
+          }
+        }
+        closedir($profiles_dir);
+        $profiles = '<a href="profiles.php?dir=' . $dir .'"> Profiles ' . "($no_profiles)" . '</a>';
+        $ascensions = '<a href="ascensions.php?dir=' . $dir . '"> Ascensions </a>';
+        
+        print( "<tr align=\"center\">  $pretty_dir <td> $hardcore </td> <td> $softcore </td> <td> $breakdown </td> <td> $profiles </td> <td> $ascensions</td> </tr>\n");
+      }
+
+      ?>
+    </table>
+
+  </body>
 </html>
